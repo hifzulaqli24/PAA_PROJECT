cb025f59657b910dfdc4d19d9547638a2356be7c
IkhsanDimas <idimastianto@student.umrah.ac.id>
2025-04-10 23:36:58 +0700

    Update main.py

--- SMART-KURIR/main.py
+++ SMART-KURIR/main.py
@@ -3,37 +3,27 @@ from engine import GameEngine
 from menu import start_menu
 
 pygame.init()
-WIDTH, HEIGHT = 1200, 800
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Smart Courier v13")
+win = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("Smart Courier v14")
 
-engine = GameEngine(win, WIDTH, HEIGHT)
+engine = GameEngine(win)
 start_menu(engine)
 
 clock = pygame.time.Clock()
-font = pygame.font.SysFont("arial", 20)
-
 while engine.running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
             engine.keluar()
 
     win.fill((255, 255, 255))
-
     if engine.map_surface:
         win.blit(engine.map_surface, (0, 0))
         engine.draw_path()
         engine.draw_flags()
         engine.draw_courier()
-
         if engine.is_moving:
             engine.move_smooth()
 
-        status_text = engine.get_status_text()
-        text = font.render(status_text, True, (0, 0, 0))
-        win.blit(text, (10, 10))
-
     pygame.display.update()
     clock.tick(60)
-
 pygame.quit()
