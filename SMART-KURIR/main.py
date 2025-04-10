74e0f711b0c7bd0333f47fb068902c2a75f97b47
hifzulaqli24 <hifzulaqli.tech@gmail.com>
2025-04-10 23:24:56 +0700

    Update main.py

--- SMART-KURIR/main.py
+++ SMART-KURIR/main.py
@@ -5,7 +5,7 @@ from menu import start_menu
 pygame.init()
 WIDTH, HEIGHT = 1200, 800
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Smart Courier v11")
+pygame.display.set_caption("Smart Courier v12")
 
 engine = GameEngine(win, WIDTH, HEIGHT)
 start_menu(engine)
@@ -23,15 +23,17 @@ while engine.running:
     if engine.map_surface:
         win.blit(engine.map_surface, (0, 0))
 
+        # Tampilkan jalur & kurir
         engine.draw_path()
         engine.draw_flags()
         engine.draw_courier()
 
+        # Jalankan pergerakan kurir
         if engine.is_moving:
             engine.move_smooth()
 
-        status = engine.get_status_text()
-        text = font.render(status, True, (0, 0, 0))
+        # Status di pojok kiri atas
+        text = font.render(engine.get_status_text(), True, (0, 0, 0))
         win.blit(text, (10, 10))
 
     pygame.display.update()
