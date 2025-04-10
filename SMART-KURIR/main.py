28d81c0f351b87c99aa2a542770d8ca1eda9fb23
hifzulaqli24 <hifzulaqli.tech@gmail.com>
2025-04-10 22:54:03 +0700

    Update main.py

--- SMART-KURIR/main.py
+++ SMART-KURIR/main.py
@@ -1,34 +1,35 @@
 import pygame
 from engine import GameEngine
+from menu import start_menu
 
 pygame.init()
 WIDTH, HEIGHT = 1200, 800
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Smart Courier - v3 (Kurir Bergerak)")
+pygame.display.set_caption("Smart Courier v5")
 
 engine = GameEngine(win, WIDTH, HEIGHT)
-engine.load_map_and_random("maps/map1.jpeg")
+start_menu(engine)
 
 clock = pygame.time.Clock()
-running = True
-
-while running:
+while engine.running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            engine.keluar()
 
     win.fill((255, 255, 255))
 
     if engine.map_surface:
         win.blit(engine.map_surface, (0, 0))
-        win.blit(engine.yellow_flag_img, engine.yellow_flag_pos)
-        win.blit(engine.red_flag_img, engine.red_flag_pos)
-
+        if engine.yellow_flag_pos:
+            win.blit(engine.yellow_flag_img, engine.yellow_flag_pos)
+        if engine.red_flag_pos:
+            win.blit(engine.red_flag_img, engine.red_flag_pos)
         if engine.courier_pos:
-            rotated_img = pygame.transform.rotate(engine.courier_img, engine.courier_angle)
-            rect = rotated_img.get_rect(center=engine.courier_pos)
-            win.blit(rotated_img, rect.topleft)
-            engine.update_courier_position()
+            rotated = pygame.transform.rotate(engine.courier_img, engine.courier_angle)
+            rect = rotated.get_rect(center=engine.courier_pos)
+            win.blit(rotated, rect.topleft)
+        if engine.is_moving:
+            engine.move_smooth()
 
     pygame.display.update()
     clock.tick(60)
