411a64904ab40c8718659093e6319da8029a78ba
Ariqa <ariqa067@gmail.com>
2025-05-18 13:21:07 +0700

    Update main.py

--- SMART-KURIR/main.py
+++ SMART-KURIR/main.py
@@ -3,7 +3,7 @@ from menu import start_menu
 from engine import GameEngine
 
 pygame.init()
-WIDTH, HEIGHT = 1200, 800
+WIDTH, HEIGHT = 1366, 700
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Smart Courier")
 
@@ -15,6 +15,9 @@ while engine.running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             engine.keluar()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            engine.handle_alert_click(event.pos)
+
 
     win.fill((255, 255, 255))
 
@@ -27,12 +30,16 @@ while engine.running:
             win.blit(engine.red_flag_img, engine.red_flag_pos)
 
         if engine.courier_pos:
+            # Bagian yang memutar gambar kurir berdasarkan arah
             rotated_img = pygame.transform.rotate(engine.courier_img, engine.courier_angle)
             rect = rotated_img.get_rect(center=engine.courier_pos)
             win.blit(rotated_img, rect.topleft)
 
         if engine.is_moving:
             engine.update_courier_position()
+
+    # ⬇️ Tambahkan ini sebelum pygame.display.update()
+    engine.draw_alert()
 
     pygame.display.update()
     clock.tick(60)
